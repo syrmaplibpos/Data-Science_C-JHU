2c20af7d7f33f6ff7deaf9b2c451079801b9bfa2
Vanilla <107902147+syrmaplibpos@users.noreply.github.com>
2022-08-16 14:52:43 -0400

    “08/16/2022”

--- Course 2 R/functions.R
+++ Course 2 R/functions.R
@@ -23,3 +23,55 @@ columnmean <- function(y, removeNA = TRUE) {
 
 
 # na.rm is a logical indicating whether missing values should be removed or not.
+
+
+
+y <- 10
+
+f <- function(x) {
+  y <- 2
+  y ^ 2 + g(x)
+}
+
+g <- function(x) {
+  x * y
+}
+
+x <- 1:10
+if (x > 5) {
+  x <- 0
+}
+
+f <- function(x) {
+  g <- function(y) {
+    y + z
+  }
+  z <- 4
+  x + g(x)
+}
+
+
+x <- 5
+y <- if(x < 3) {
+  NA
+} else {
+  10
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
