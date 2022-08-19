6ff2d43cb222eb61899b765dc07dc67809d87bee
Vanilla <107902147+syrmaplibpos@users.noreply.github.com>
2022-08-18 22:52:42 -0400

    “08/18/2022”

---
+++ Course 2 R/pollutantmean.R
@@ -0,0 +1,10 @@
+complete <- function(directory, id = 1:332) {
+  location <- paste(getwd(),directory,sep = "/", collapse=NULL)
+  print(location)
+  list.files(path = location, pattern = NULL)
+  paste(,sep = "", collapse = NULL)
+  data <- read.csv()
+}
+
+
+
