f97c44e87d5285db6e2fc1d8d8d28048966472f0
Vanilla <107902147+syrmaplibpos@users.noreply.github.com>
2022-09-08 16:02:43 -0700

    09/08/2022

--- Course 2 R/pollutantmean.R
+++ Course 2 R/pollutantmean.R
@@ -2,7 +2,7 @@ complete <- function(directory, id = 1:332) {
   location <- paste(getwd(),directory,sep = "/", collapse=NULL)
   print(location)
   list.files(path = location, pattern = NULL)
-  paste(,sep = "", collapse = NULL)
+  p <- paste(,sep = "", collapse = NULL)
   data <- read.csv()
 }
 
